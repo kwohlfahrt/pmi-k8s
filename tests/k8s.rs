138c364a5626d675966e323ee4abda63202b47ce
Kai Wohlfahrt <kai@vant.ai>
2026-02-10 19:50:40 +0100

    Kubernetes tests
    This requires some manual editing of the kubeconfig to work in
    devcontainers.

---
+++ tests/k8s.rs
@@ -0,0 +1,55 @@
+use std::{
+    env,
+    path::Path,
+    process::{Command, ExitStatus},
+    time::Duration,
+};
+
+struct Kustomization<'a> {
+    path: &'a Path,
+}
+
+impl<'a> Kustomization<'a> {
+    fn new(path: &'a Path) -> Self {
+        Command::new("kubectl")
+            .args(["apply", "-k", path.to_str().unwrap()])
+            .status()
+            .unwrap();
+
+        Self { path }
+    }
+}
+
+impl<'a> Drop for Kustomization<'a> {
+    fn drop(&mut self) {
+        Command::new("kubectl")
+            .args(["delete", "-k", self.path.to_str().unwrap()])
+            .status()
+            .unwrap();
+    }
+}
+
+fn wait_for_complete(name: &str, timeout: Duration) -> ExitStatus {
+    Command::new("kubectl")
+        .args(["wait", "--for", "condition=Complete"])
+        .arg(format!("--timeout={}s", timeout.as_secs()))
+        .arg(&format!("jobs.batch/{}", name))
+        .status()
+        .unwrap()
+}
+
+#[tokio::test]
+async fn test_fence() {
+    let path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("tests/kustomization/base");
+    let _k = Kustomization::new(&path);
+
+    assert!(wait_for_complete("pmi-k8s-test", Duration::from_mins(1)).success())
+}
+
+#[tokio::test]
+async fn test_modex() {
+    let path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("tests/kustomization/dmodex");
+    let _k = Kustomization::new(&path);
+
+    assert!(wait_for_complete("pmi-k8s-test-dmodex", Duration::from_mins(1)).success())
+}
--- tests/test.py
+++ tests/test.py
@@ -4,17 +4,16 @@ from mpi4py import MPI
 import numpy as np
 import sys
 
-size = int(sys.argv[1])
+expected_size = int(sys.argv[1])
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-print("size:", comm.Get_size(), "rank:", rank)
+size = comm.Get_size()
+assert size == expected_size
 
 sendbuf = np.full(10, rank, dtype='i')
 recvbuf = np.empty([size, 10], dtype='i')
 comm.Allgather(sendbuf, recvbuf)
 
-print(recvbuf)
-
-for i in range(size):
-    assert np.allclose(recvbuf[i,:], i)
+expected = np.arange(size)[:, np.newaxis]
+assert np.allclose(expected, recvbuf)
