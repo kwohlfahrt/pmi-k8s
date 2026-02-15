dde4fb6eeb53bf4ca0ea9012d529c381e990baa5
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2026-02-15 13:53:20 +0100

    Fix most lints in main

--- src/main.rs
+++ src/main.rs
@@ -13,56 +13,47 @@ use pmi_k8s::{
     pmix,
 };
 
+const WILDCARD: net::IpAddr = net::IpAddr::V4(net::Ipv4Addr::new(0, 0, 0, 0));
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Error> {
     let args = Cli::parse();
     let namespace = c"foo";
 
     let peers = KubernetesPeers::new().await?;
-    let fence = NetFence::new(
-        net::SocketAddr::new("0.0.0.0".parse().unwrap(), PORT),
-        &peers,
-    )
-    .await
-    .unwrap();
-    let modex = NetModex::new(
-        net::SocketAddr::new("0.0.0.0".parse().unwrap(), PORT + 1),
-        &peers,
-        args.nproc,
-    )
-    .await
-    .unwrap();
+    let fence = NetFence::new(net::SocketAddr::new(WILDCARD, PORT), &peers).await?;
+    let modex = NetModex::new(net::SocketAddr::new(WILDCARD, PORT + 1), &peers, args.nproc).await?;
 
     let hostnames = peers.hostnames().collect::<Vec<_>>();
     let hostname_refs = hostnames.iter().map(|h| h.as_c_str()).collect::<Vec<_>>();
 
-    let tempdir = TempDir::new("pmi-k8s").unwrap();
-    let s = pmix::server::Server::init(fence, modex, tempdir.path()).unwrap();
+    let tempdir = TempDir::new("pmi-k8s")?;
+    let s = pmix::server::Server::init(fence, modex, tempdir.path())?;
     let ns = pmix::server::Namespace::register(&s, namespace, &hostname_refs, args.nproc);
     let clients = peers
         .local_ranks(args.nproc)
-        .map(|i| pmix::server::Client::register(&ns, i as u32))
+        .map(|i| pmix::server::Client::register(&ns, i))
         .collect::<Vec<_>>();
 
     let ps = clients
         .iter()
         .map(|c| {
             let mut cmd = Command::new(&args.command);
-            cmd.envs(&c.envs()).args(&args.args).spawn().unwrap()
+            cmd.envs(&c.envs()).args(&args.args).spawn()
         })
-        .collect::<Vec<_>>();
+        .collect::<Result<Vec<_>, _>>()?;
 
     let rcs = tokio::task::spawn_blocking(|| {
         ps.into_iter()
-            .map(|mut p| p.wait().unwrap())
-            .collect::<Vec<_>>()
+            .map(|mut p| p.wait())
+            .collect::<Result<Vec<_>, _>>()
     });
     let run = pin!(s.run());
     let Either::Left((rcs, _)) = select(rcs, run).await else {
         panic!("server stopped unexpectedly")
     };
 
-    assert!(rcs.unwrap().iter().all(|rc| rc.success()));
+    assert!(rcs??.iter().all(|rc| rc.success()));
 
     Ok(())
 }
