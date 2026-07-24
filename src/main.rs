16c965d057b85eac308aef493117e192b1b0618f
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2026-07-24 08:48:07 +0200

    Sidecar container support
    This works! The remaining issue is that the pmi-k8s sidecar does not
    respond to SIGTERM, so it remains until the 30s grace period expires and
    it is killed.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use futures::{
-    TryStreamExt,
-    future::{Either, select},
-    stream::FuturesUnordered,
+    StreamExt, TryStreamExt,
+    future::{self, Either},
+    stream::{self, FuturesUnordered},
 };
 use std::{net, pin::pin};
 use tempdir::TempDir;
 
 use anyhow::Error;
 use clap::Parser;
-use tokio::process::Command;
+use tokio::{fs, process::Command};
 
 use pmi_k8s::{
     Cli,
@@ -41,17 +41,39 @@ async fn main() -> Result<(), Error> {
         .collect::<Result<Vec<_>, _>>()?;
 
     let run = pin!(e.run(fence, modex));
-    let rcs = clients
-        .iter()
-        .map(async |c| -> Result<_, Error> {
-            let envs = c.envs()?;
-            let mut cmd = Command::new(&args.command);
-            Ok(cmd.envs(&envs).args(&args.args).spawn()?.wait().await?)
-        })
-        .collect::<FuturesUnordered<_>>()
-        .try_collect::<Vec<_>>();
 
-    let rcs = match select(rcs, run).await {
+    let envs = clients
+        .iter()
+        .map(|c| c.envs())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    if let Some(env_path) = args.env_dir {
+        stream::iter(envs.iter())
+            .enumerate()
+            .map(Ok::<_, Error>)
+            .try_for_each(async |(i, envs)| {
+                let env_path = env_path.join(format!("{}.env", i));
+                let mut file = fs::File::create(&env_path).await?;
+                Ok(envs.write(&mut file).await?)
+            })
+            .await?;
+
+        fs::File::create(env_path.join("ready")).await?;
+    }
+
+    let rcs = if let Some(command) = args.command {
+        Either::Left(
+            envs.into_iter()
+                .map(|envs| Command::new(&command).envs(&envs).args(&args.args).spawn())
+                .map(async |spawn| Ok::<_, Error>(spawn?.wait().await?))
+                .collect::<FuturesUnordered<_>>()
+                .try_collect::<Vec<_>>(),
+        )
+    } else {
+        Either::Right(future::pending())
+    };
+
+    let rcs = match future::select(rcs, run).await {
         Either::Left((rcs, _)) => rcs?,
         Either::Right((Ok(()), rcs)) => rcs.await?,
         Either::Right((Err(err), _)) => Err(err)?,
